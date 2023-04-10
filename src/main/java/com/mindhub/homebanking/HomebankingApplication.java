c6fd2ac293917faa3d805b14af58b8ce2ac33e3e
Jennifer <jmota@fi.uba.ar>
2023-04-10 18:10:34 -0300

    sout

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -21,6 +21,7 @@ public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+		System.out.println("Hola");
 	}
 
 	LocalDateTime today =  LocalDateTime.now();
